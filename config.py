9d21502bb094d97c88a1cb1c936b1cfc1cf9b084
alexalaver <alex.alaverdyan.04@gmail.com>
2023-10-24 19:18:41 +0400

    beta 0.2 interface

--- config.py
+++ config.py
@@ -1,1 +1,11 @@
 TOKEN = "6537283600:AAElfw2nNoqVgeABFy_opd48mUKAPPl62FA"
+def profile(profile_id, balance, tariffe):
+    return f"Ваш идентификатор: {profile_id}\nТекущий баланс: {balance}\nВаш тариф: {tariffe}"
+
+up_balance = "Пополнение баланса"
+tariff_selection = "Выбор тарифа"
+
+autoposting = "Автопостинг"
+parser = "Парсер"
+my_profile = "Мой профиль"
+support = "Саппорт"
--- main.py
+++ main.py
@@ -10,7 +10,18 @@ dp = Dispatcher(bot)
 @dp.message_handler(commands=['start'])
 async def start(message: types.Message):
     if message.chat.type == types.ChatType.PRIVATE:
-        await message.answer('test')
+        user_id = message.from_user.id
+        markup_inline = types.InlineKeyboardMarkup(row_width=1)
+        btn_inline1 = types.InlineKeyboardButton(cfg.up_balance, callback_data='up_balance')
+        btn_inline2 = types.InlineKeyboardButton(cfg.tariff_selection, callback_data='tariff_selection')
+        markup_inline.add(btn_inline1, btn_inline2)
+        markup_reply = types.ReplyKeyboardMarkup(row_width=2, resize_keyboard=True, one_time_keyboard=True)
+        markup_reply.add(cfg.autoposting)
+        markup_reply.add(cfg.parser)
+        markup_reply.row(cfg.my_profile, cfg.support)
+
+        await message.answer('test', reply_markup=markup_reply)
+        await message.answer_photo(photo=types.InputFile("img/testphoto.png"), caption=cfg.profile(user_id, 'test', 'test'), reply_markup=markup_inline)
 
 
 if __name__ == "__main__":
