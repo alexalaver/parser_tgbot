985ff4b0db6b3e02d7e34391c5a70c2963f7b319
alexalaver <alex.alaverdyan.04@gmail.com>
2023-10-27 02:04:43 +0400

    beta 0.5 add button tariffe list and button back + bugfix

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 TOKEN = "6537283600:AAElfw2nNoqVgeABFy_opd48mUKAPPl62FA"
 def profile(profile_id, balance, tariffe):
-    return f"Ваш идентификатор: {profile_id}\nТекущий баланс: {balance}\nВаш тариф: {tariffe}"
+    return f"*Ваш идентификатор: *{profile_id}\n*Текущий баланс: *{balance}\n*Ваш тариф: *{tariffe}"
 
 up_balance = "Пополнение баланса"
 tariff_selection = "Выбор тарифа"
@@ -9,3 +9,9 @@ autoposting = "Автопостинг"
 parser = "Парсер"
 my_profile = "Мой профиль"
 support = "Саппорт"
+
+tariff_list = "*Ниже предоставлены основные тарифы.\nМаксимальное количество чатов для парсинга - 50,\nесли вам необходимо больше - свяжитесь с саппортом*"
+start_tariff_button = "Стартовый 10 чатов - 15$"
+standart_tariff_button = "Стандарт 25 чатов - 39$"
+premium_tariff_button = "Премиум 50 чатов - 69$"
+back_button = "Назад"
--- main.py
+++ main.py
@@ -1,13 +1,25 @@
 from aiogram import Bot, Dispatcher, types, executor
+from aiogram.dispatcher.filters.state import State, StatesGroup
+from aiogram.dispatcher.storage import FSMContext
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
 from datbas import Data
 import config as cfg
 import logging
 
 logging.basicConfig(level=logging.INFO)
 
-bot = Bot(cfg.TOKEN)
-dp = Dispatcher(bot)
+bot = Bot(cfg.TOKEN, parse_mode=types.ParseMode.MARKDOWN)
+dp = Dispatcher(bot, storage=MemoryStorage())
 db = Data("localhost", "5432", "parsers", "parser_user", "parser_pwd")
+
+
+async def profile(message):
+    user_id = message.from_user.id
+    markup_inline = types.InlineKeyboardMarkup(row_width=1)
+    btn_inline1 = types.InlineKeyboardButton(cfg.up_balance, callback_data='up_balance')
+    btn_inline2 = types.InlineKeyboardButton(cfg.tariff_selection, callback_data='tariff_selection')
+    markup_inline.add(btn_inline1, btn_inline2)
+    await message.answer_photo(photo=types.InputFile("img/testphoto.png"), caption=cfg.profile(user_id, 'test', 'test'), reply_markup=markup_inline)
 
 @dp.message_handler(commands=['start'])
 async def start(message: types.Message):
@@ -17,18 +29,41 @@ async def start(message: types.Message):
         username = message.from_user.username
         if(not db.check_user(user_id)):
             db.add_user(user_id, first_name, username)
-        markup_inline = types.InlineKeyboardMarkup(row_width=1)
-        btn_inline1 = types.InlineKeyboardButton(cfg.up_balance, callback_data='up_balance')
-        btn_inline2 = types.InlineKeyboardButton(cfg.tariff_selection, callback_data='tariff_selection')
-        markup_inline.add(btn_inline1, btn_inline2)
         markup_reply = types.ReplyKeyboardMarkup(row_width=2, resize_keyboard=True, one_time_keyboard=True)
         markup_reply.add(cfg.autoposting)
         markup_reply.add(cfg.parser)
         markup_reply.row(cfg.my_profile, cfg.support)
 
         await message.answer('test', reply_markup=markup_reply)
-        await message.answer_photo(photo=types.InputFile("img/testphoto.png"), caption=cfg.profile(user_id, 'test', 'test'), reply_markup=markup_inline)
+        await profile(message)
 
+@dp.callback_query_handler()
+async def buttons_callback(callback_query: types.CallbackQuery):
+    if callback_query.data == "tariff_selection":
+        markup_inline = types.InlineKeyboardMarkup(row_width=1)
+        btn_inline1 = types.InlineKeyboardButton(cfg.start_tariff_button, callback_data='start_tariff')
+        btn_inline2 = types.InlineKeyboardButton(cfg.standart_tariff_button, callback_data='standart_tariff')
+        btn_inline3 = types.InlineKeyboardButton(cfg.premium_tariff_button, callback_data='premium_tariff')
+        btn_inline4 = types.InlineKeyboardButton(cfg.my_profile, callback_data='profile')
+        btn_inline5 = types.InlineKeyboardButton(cfg.support, callback_data='support')
+        btn_inline6 = types.InlineKeyboardButton(cfg.back_button, callback_data='back_tariff')
+        markup_inline.add(btn_inline1, btn_inline2, btn_inline3)
+        markup_inline.row(btn_inline4, btn_inline5)
+        markup_inline.add(btn_inline6)
+        await callback_query.message.edit_caption(caption=cfg.tariff_list, reply_markup=markup_inline)
+    elif callback_query.data == "back_tariff":
+        user_id = callback_query.from_user.id
+        markup_inline = types.InlineKeyboardMarkup(row_width=1)
+        btn_inline1 = types.InlineKeyboardButton(cfg.up_balance, callback_data='up_balance')
+        btn_inline2 = types.InlineKeyboardButton(cfg.tariff_selection, callback_data='tariff_selection')
+        markup_inline.add(btn_inline1, btn_inline2)
+        await callback_query.message.edit_caption(caption=cfg.profile(user_id, 'test', 'test'), reply_markup=markup_inline)
+
+@dp.message_handler()
+async def other(message: types.Message):
+    if message.chat.type == types.ChatType.PRIVATE:
+        if message.text == cfg.my_profile:
+            await profile(message)
 
 if __name__ == "__main__":
     executor.start_polling(dp)
