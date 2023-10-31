c70a602aaeb1e1e1a22457f0d85842b1a1aaa6c0
alexalaver <alex.alaverdyan.04@gmail.com>
2023-11-01 00:08:21 +0400

    beta 3.1 bug fix

--- datbas.py
+++ datbas.py
@@ -99,10 +99,11 @@ class Data:
         with self.connect:
             self.cursor.execute("SELECT id FROM groups WHERE id=%s", (id,))
             a = self.cursor.fetchone()
+            b = [row for row in a]
             if a is None:
                 return 0
             else:
-                return len(a)
+                return len(b)
 
     def add_channels(self, id, number_group, keyword, channels, group_name):
         with self.connect:
