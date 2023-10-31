aca3760ee673d14203be75ce8e156148dc3c74ef
alexalaver <alex.alaverdyan.04@gmail.com>
2023-11-01 01:07:59 +0400

    beta 3.1 bug fix

--- main.py
+++ main.py
@@ -185,27 +185,22 @@ async def buttons_callback(callback_query: types.CallbackQuery):
             markup_inline = types.InlineKeyboardMarkup()
             page = 0
 
-            # Функция для создания кнопок для текущей страницы
-            def create_buttons(page, items):
-                buttons = []
-                start = page * buttons_per_page
-                end = start + buttons_per_page
-                for i in range(start, min(end, len(items))):
-                    button = types.InlineKeyboardButton(items[i], callback_data=items[i])
-                    buttons.append(button)
-                return buttons
+            # Создаем кнопки для текущей страницы
+            buttons = []
+            start = page * buttons_per_page
+            end = start + buttons_per_page
+            for i in range(start, min(end, len(channels))):
+                button = types.InlineKeyboardButton(channels[i], callback_data=channels[i])
+                buttons.append(button)
 
-            # Функция для обновления клавиатуры
-            def update_markup(page, channels):
-                markup_inline = types.InlineKeyboardMarkup()
-                buttons = create_buttons(page, channels)
-                markup_inline.add(*buttons)
-                prev_button = types.InlineKeyboardButton("<<", callback_data=f"prev_{page - 1}")
-                next_button = types.InlineKeyboardButton(">>", callback_data=f"next_{page + 1}")
-                markup_inline.add(prev_button, next_button)
-                return markup_inline
+            markup_inline.add(*buttons)
 
-            markup_inline = update_markup(page, channels)
+            # Добавляем кнопки "вперед" и "назад"
+            prev_button = types.InlineKeyboardButton("<<", callback_data=f"prev_{page - 1}")
+            next_button = types.InlineKeyboardButton(">>", callback_data=f"next_{page + 1}")
+
+            markup_inline.add(prev_button, next_button)
+
             await callback_query.message.edit_caption(caption='TESTING', reply_markup=markup_inline)
 
 
