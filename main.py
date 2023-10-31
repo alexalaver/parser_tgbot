17003f6887c5264a118beb92b385f94356af8fd4
alexalaver <alex.alaverdyan.04@gmail.com>
2023-11-01 00:16:42 +0400

    beta 3.1 bug fix

--- main.py
+++ main.py
@@ -168,7 +168,7 @@ async def buttons_callback(callback_query: types.CallbackQuery):
                 await Create_group.create_group_1.set()
                 await callback_query.message.answer(cfg.create_group_text_1)
                 await callback_query.message.answer(cfg.create_group_text_2, reply_markup=markup_reply)
-                await callback_query.answer(cfg.create_group_button_uved)
+                await callback_query.answer(cfg.create_group_button_uved, show_alert=True)
                 db.delete_cashe_parsing(user_id)
             else:
                 await callback_query.answer(cfg.error_group_5)
