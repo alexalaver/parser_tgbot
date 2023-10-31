c70535c44f1b648224038aeeefa4202c22c82ec0
alexalaver <alex.alaverdyan.04@gmail.com>
2023-10-31 18:38:52 +0400

    beta 2.1 create groups

--- datbas.py
+++ datbas.py
@@ -116,7 +116,7 @@ class Data:
 
     def add_cashe_keyword_parsing(self, id, keyword):
         with self.connect:
-            self.cursor.execute("UPDATE cash_parsing SET keyword=%s ", (id, keyword,))
+            self.cursor.execute("UPDATE cash_parsing SET keyword=%s ", (keyword,))
             self.connect.commit()
 
     def delete_cashe_parsing(self, id):
