de104fc3c6ea8a321dfc75f2bfd02d35c4fbc796
alexalaver <alex.alaverdyan.04@gmail.com>
2023-10-31 19:18:35 +0400

    beta 2.3 bug fix

--- main.py
+++ main.py
@@ -162,7 +162,7 @@ async def buttons_callback(callback_query: types.CallbackQuery):
         user_id = callback_query.from_user.id
         if callback_query.data == "groups_add_button":
             number_group = db.check_number_group(user_id)
-            if 5 >= number_group:
+            if 5 > number_group:
                 markup_reply = types.ReplyKeyboardMarkup(row_width=1, resize_keyboard=True)
                 markup_reply.add(cfg.cancel_creategroup)
                 await Create_group.create_group_1.set()
