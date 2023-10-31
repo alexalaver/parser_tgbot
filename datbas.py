463330ed8dbf491bcfc92994f3c4d778a1d89738
alexalaver <alex.alaverdyan.04@gmail.com>
2023-10-31 23:58:37 +0400

    beta 3.1 bug fix

--- datbas.py
+++ datbas.py
@@ -98,11 +98,11 @@ class Data:
     def check_number_group(self, id):
         with self.connect:
             self.cursor.execute("SELECT id FROM groups WHERE id=%s", (id,))
-            a = self.cursor.fetchone()[0]
+            a = self.cursor.fetchone()
             if a is None:
                 return 0
             else:
-                return len(str(a))
+                return len(str(a[0]))
 
     def add_channels(self, id, number_group, keyword, channels, group_name):
         with self.connect:
