803407cb7537374585e862f7f5fbb8a81df6c944
alexalaver <alex.alaverdyan.04@gmail.com>
2023-10-31 18:34:42 +0400

    beta 2.1 create groups

--- datbas.py
+++ datbas.py
@@ -98,7 +98,7 @@ class Data:
     def check_number_group(self, id):
         with self.connect:
             self.cursor.execute("SELECT id FROM groups WHERE id=%s", (id,))
-            a = self.cursor.fetchall()[0]
+            a = self.cursor.fetchall()
             if a is None:
                 return 0
             else:
