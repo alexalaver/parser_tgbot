f4ff4290e75bf8409b5e3ebd5d894695121a870c
alexalaver <alex.alaverdyan.04@gmail.com>
2023-10-31 18:53:23 +0400

    beta 2.1 create groups

--- datbas.py
+++ datbas.py
@@ -127,6 +127,6 @@ class Data:
     def select_cashe_parsing(self, id):
         with self.connect:
             self.cursor.execute("SELECT group_name, keyword FROM cash_parsing WHERE id=%s", (id,))
-            result = self.cursor.fetchone
+            result = self.cursor.fetchone()
             group_name, keyword = result
             return [group_name, keyword]
