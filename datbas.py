7c0f3306d7147a304db04e56a3830d0c5d7af1b1
alexalaver <alex.alaverdyan.04@gmail.com>
2023-11-02 18:56:35 +0400

    beta 10.0 add pay

--- datbas.py
+++ datbas.py
@@ -87,8 +87,11 @@ class Data:
     def check_date_tarife_for_number(self, id, number_group):
         with self.connect:
             self.cursor.execute("SELECT data_end FROM groups WHERE id=%s AND number_group=%s", (id, number_group,))
-            a = self.cursor.fetchone()[0]
-            return a
+            a = self.cursor.fetchone()
+            if a is None:
+                return None
+            else:
+                return a[0]
 
     def delete_old_tariffe(self, id, number_group):
         with self.connect:
