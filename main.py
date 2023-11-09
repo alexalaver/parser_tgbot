5d4fe028ea79d457e3966bca86b7d2868d2e3362
alexalaver <alex.alaverdyan.04@gmail.com>
2023-11-10 00:38:14 +0400

    bugfixes

--- main.py
+++ main.py
@@ -270,7 +270,7 @@ async def buttons_callback(callback_query: types.CallbackQuery, state: FSMContex
                     buttons = types.InlineKeyboardButton(text=f"{channel} ✅", callback_data=f"{channel} ✅")
                     markup_inline.row(buttons)
                 for off_channel in off_channels:
-                    buttons_1 = types.InlineKeyboardButton(text=f"{off_channel} ❌", callback_data=f"{off_channel} ❌")
+                    buttons_1 = types.InlineKeyboardButton(text=f"{off_channel} X", callback_data=f"{off_channel} X")
                     markup_inline.row(buttons_1)
             back_channels = types.InlineKeyboardButton(text=cfg.back_channels, callback_data='back_channels')
             markup_inline.add(back_channels)
@@ -325,12 +325,12 @@ async def parsers_use_1_button(callback_query: types.CallbackQuery, state: FSMCo
                             buttons = types.InlineKeyboardButton(text=f"{channel} ✅", callback_data=f"{channel} ✅")
                             markup_inline.row(buttons)
                         for off_channel in off_channels:
-                            buttons_1 = types.InlineKeyboardButton(text=f"{off_channel} ❌", callback_data=f"{off_channel} ❌")
+                            buttons_1 = types.InlineKeyboardButton(text=f"{off_channel} X", callback_data=f"{off_channel} X")
                             markup_inline.row(buttons_1)
                         back_channels = types.InlineKeyboardButton(text=cfg.back_channels, callback_data='back_channels')
                         markup_inline.add(back_channels)
                         await callback_query.message.edit_caption(caption="TESTING", reply_markup=markup_inline)
-                    elif channel_name[-1] == "❌":
+                    elif channel_name[-1] == "X":
                         channel_name = channel_name[:-1].strip()
                         off_channels = db.select_off_channels(number_group)
                         off_channels.remove(channel_name)
@@ -345,7 +345,7 @@ async def parsers_use_1_button(callback_query: types.CallbackQuery, state: FSMCo
                             buttons = types.InlineKeyboardButton(text=f"{channel} ✅", callback_data=f"{channel} ✅")
                             markup_inline.row(buttons)
                         for off_channel in off_channels:
-                            buttons_1 = types.InlineKeyboardButton(text=f"{off_channel} ❌", callback_data=f"{off_channel} ❌")
+                            buttons_1 = types.InlineKeyboardButton(text=f"{off_channel} X", callback_data=f"{off_channel} X")
                             markup_inline.row(buttons_1)
                         back_channels = types.InlineKeyboardButton(text=cfg.back_channels, callback_data='back_channels')
                         markup_inline.add(back_channels)
