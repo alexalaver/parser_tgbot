2823d9c27923e44e7669cf0f612e96820e592f4e
alexalaver <alex.alaverdyan.04@gmail.com>
2023-11-09 23:32:09 +0400

    bugfixs

--- main.py
+++ main.py
@@ -304,7 +304,10 @@ async def parsers_use_1_button(callback_query: types.CallbackQuery, state: FSMCo
                     if channel_name[-1] == "✅":
                         channel_name = channel_name[:-1].strip()
                         all_channels = db.select_channels_number_group(number_group)
-                        all_channels.remove(channel_name)
+                        if channel_name in all_channels:
+                            all_channels.remove(channel_name)
+                        else:
+                            print(f"\n\nОШИБКААААААААААААААААААААААААААА\n\n{all_channels}\n\n{channel_name}\n\n")
                         off_channels = db.select_off_channels(number_group)
                         off_channels.append(channel_name)
                         db.update_all_channels(user_id, all_channels)
