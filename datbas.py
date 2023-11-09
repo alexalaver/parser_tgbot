9885a27b73a851a1ba3f35400e037a14a6270df0
alexalaver <alex.alaverdyan.04@gmail.com>
2023-11-10 01:19:08 +0400

    bugfixes

--- datbas.py
+++ datbas.py
@@ -167,7 +167,7 @@ class Data:
     def select_off_channels_with_number(self, number_group):
         with self.connect:
             self.cursor.execute("SELECT off_channels FROM groups WHERE number_group=%s", (number_group,))
-            a = self.cursor.fetchone()
+            a = self.cursor.fetchone()[0]
             if a is None:
                 return []
             else:
