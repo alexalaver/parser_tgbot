32db7eace665f649efffa596016b8d4ef6403b4a
alexalaver <alex.alaverdyan.04@gmail.com>
2023-11-09 22:46:09 +0400

    bugfix

--- main.py
+++ main.py
@@ -290,7 +290,7 @@ async def parsers_use_1_button(callback_query: types.CallbackQuery, state: FSMCo
         channels = db.select_channels_with_number(user_id, number_group)
         check_tarife = db.check_date_tarife_for_number(user_id, number_group)
         group_name = db.select_group_name_for_number_group(user_id, number_group)
-        await callback_query.message.answer(channels)
+        await callback_query.message.answer(callback_query.data[:-2])
         if callback_query.data[:-2] in channels:
             await callback_query.message.answer(callback_query.data[:-2])
             if check_tarife is None:
