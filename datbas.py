c53689113a6d851fcfbe6e9e4786fa535e9d76fd
alexalaver <alex.alaverdyan.04@gmail.com>
2023-11-09 22:59:45 +0400

    bugfix

--- datbas.py
+++ datbas.py
@@ -208,5 +208,6 @@ class Data:
     def select_off_channels(self, id):
         with self.connect:
             self.cursor.execute("SELECT off_channels FROM groups WHERE id=%s", (id,))
-            a = self.cursor.fetchone()[0]
-            return a
+            a = self.cursor.fetchall()
+            combined_list = list(itertools.chain(*a))
+            return combined_list
--- main.py
+++ main.py
@@ -302,10 +302,19 @@ async def parsers_use_1_button(callback_query: types.CallbackQuery, state: FSMCo
                 else:
                     channel_name = callback_query.data
                     if channel_name[-1] == "✅":
-                        off_channels_list = []
+                        all_channels = db.select_all_channels(user_id)
+                        all_channels.remove(channel_name[:-2])
                         off_channels = db.select_off_channels(user_id)
-                        off_channels_list.append(off_channels)
-                        await callback_query.message.answer("test")
+                        off_channels.append(channel_name[:-2])
+                        await state.update_data(number_group=number_group)
+                        channels = db.select_channels(user_id, callback_query.data)
+                        markup_inline = types.InlineKeyboardMarkup(row_width=4)
+                        for channel in channels:
+                            buttons = types.InlineKeyboardButton(text=f"{channel} ✅", callback_data=f"{channel} ✅")
+                            markup_inline.row(buttons)
+                        back_channels = types.InlineKeyboardButton(text=cfg.back_channels, callback_data='back_channels')
+                        markup_inline.add(back_channels)
+                        await callback_query.message.edit_caption(caption="TESTING", reply_markup=markup_inline)
         elif callback_query.data == "back_channels":
             await state.reset_state()
             markup_inline = types.InlineKeyboardMarkup(row_width=1)
