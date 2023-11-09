1780c50b217f5097dc179f69c284899ea0d81d12
alexalaver <alex.alaverdyan.04@gmail.com>
2023-11-10 01:14:47 +0400

    bugfixes

--- main.py
+++ main.py
@@ -296,7 +296,7 @@ async def parsers_use_1_button(callback_query: types.CallbackQuery, state: FSMCo
         data = await state.get_data()
         number_group = data.get('number_group')
         channels = db.select_channels_with_number(number_group)
-        off_channels = db.select_off_channels_with_number(number_group)
+        off_channels = db.select_off_channels_with_number(number_group) or []
         check_tarife = db.check_date_tarife_for_number(user_id, number_group)
         group_name = db.select_group_name_for_number_group(user_id, number_group)
         if callback_query.data[:-2] in channels or callback_query.data[:-2] in off_channels:
