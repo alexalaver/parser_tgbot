8126d038eb696ea6edcd315383cbf3353d913910
alexalaver <alex.alaverdyan.04@gmail.com>
2023-11-10 18:32:40 +0400

    new_version optimizated

--- main.py
+++ main.py
@@ -35,7 +35,7 @@ async def check_for_new_channels(current_count):
 async def search_and_forward():
     num = 0
     last_message_ids = {}
-    messages_sent = {}
+    messages_sent = set()
 
     await telethon_client.start()
     groups_count = len(db.select_all_channels_group())
@@ -66,11 +66,15 @@ async def search_and_forward():
                 try:
                     async for message in telethon_client.iter_messages(chat_id[:-2], offset_id=last_id - messages_to_check, limit=messages_to_check, reverse=True):
                         if message.text and any(keyword.lower() in message.text.lower() for keyword in keywords):
-                            message_key = (user_id, message.id)
+                            message_key = f"{chat_id[:-2]}_{message.id}"
                             if message_key not in messages_sent or forced_check:
                                 await bot.send_message(user_id, message.text, parse_mode=types.ParseMode.MARKDOWN)
                                 print(f"Message sent to user {user_id}: {message.text}")
-                                messages_sent[message_key] = True
+                                messages_sent.add(message_key)
+
+                    messages = await telethon_client.get_messages(chat_id[:-2], limit=1)
+                    if messages:
+                        last_message_ids[chat_id[:-2]] = messages[0].id
 
                 except FloodWaitError as e:
                     wait_time = e.seconds
