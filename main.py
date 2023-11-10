7eeef0634ab627c67e665b5cd008afeceb96af5b
alexalaver <alex.alaverdyan.04@gmail.com>
2023-11-11 01:12:37 +0400

    pages realize version

--- main.py
+++ main.py
@@ -78,7 +78,7 @@ async def search_and_forward():
                                 sender_identifier = f"@{sender.username}" if sender and sender.username else "Анонимный пользователь"
 
                                 message_text = f"{message.text}\n\nИз чата {chat_identifier}\n\nСообщение от {sender_identifier}"
-                                await bot.send_message(user_id, message_text, parse_mode=types.ParseMode.MARKDOWN)
+                                await bot.send_message(user_id, message_text, parse_mode=types.ParseMode.HTML)
                                 print(f"Message sent to user {user_id}: {message.text}")
                                 messages_sent[message_key] = True
 
