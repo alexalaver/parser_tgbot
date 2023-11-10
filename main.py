54661bcc5197a01676d2f432b166157f6b495d75
alexalaver <alex.alaverdyan.04@gmail.com>
2023-11-10 20:24:09 +0400

    new_version optimizated

--- main.py
+++ main.py
@@ -271,11 +271,14 @@ async def buttons_callback(callback_query: types.CallbackQuery, state: FSMContex
             markup_inline = types.InlineKeyboardMarkup(row_width=2)
             channels_count = len(channels)
             channels_page = fnc.get_category(channels_count)
+            page_here = 1
+            await state.update_data(channels_count=channels_count)
             await state.update_data(channels_page=channels_page)
+            await state.update_data(page_here=page_here)
             for channel in channels[:10]:
                 buttons = types.InlineKeyboardButton(text=channel, callback_data=channel)
                 markup_inline.row(buttons)
-            buttons_count = types.InlineKeyboardButton(text=f"Страница 1/{channels_page}", callback_data="page")
+            buttons_count = types.InlineKeyboardButton(text=f"Страница {page_here}/{channels_page}", callback_data="page")
             markup_inline.add(buttons_count)
             if channels_count > 10:
                 buttons_next = types.InlineKeyboardButton(text=cfg.next_page, callback_data="next_page")
@@ -310,6 +313,9 @@ async def parsers_use_1_button(callback_query: types.CallbackQuery, state: FSMCo
         channels = db.select_channels_with_number(number_group)
         check_tarife = db.check_date_tarife_for_number(number_group)
         group_name = db.select_group_name_for_number_group(user_id, number_group)
+        channels_count = data.get("channels_count")
+        channels_page = data.get("channels_page")
+        page_here = data.get("page_here")
         if callback_query.data in channels:
             if check_tarife is None:
                 await callback_query.answer(text=cfg.error_oplata, show_alert=True)
@@ -339,6 +345,55 @@ async def parsers_use_1_button(callback_query: types.CallbackQuery, state: FSMCo
                     back_channels = types.InlineKeyboardButton(text=cfg.back_channels, callback_data='back_channels')
                     markup_inline.add(back_channels)
                     await callback_query.message.edit_caption(caption=cfg.group_text_use, reply_markup=markup_inline, parse_mode=types.ParseMode.MARKDOWN)
+        elif callback_query.data == "next_page":
+            if channels_page == page_here:
+                await callback_query.answer(cfg.error_page_next, show_alert=True)
+            else:
+                channels = db.select_channels_with_number(number_group)
+                markup_inline = types.InlineKeyboardMarkup(row_width=2)
+                channels_count = len(channels)
+                channels_page = fnc.get_category(channels_count)
+                page_here = 1
+                for channel in channels[:channels_count - 10]:
+                    buttons = types.InlineKeyboardButton(text=channel, callback_data=channel)
+                    markup_inline.row(buttons)
+                buttons_count = types.InlineKeyboardButton(text=f"Страница {page_here}/{channels_page}",
+                                                           callback_data="page")
+                markup_inline.add(buttons_count)
+                if channels_count > 10:
+                    buttons_next = types.InlineKeyboardButton(text=cfg.next_page, callback_data="next_page")
+                    buttons_old = types.InlineKeyboardButton(text=cfg.old_page, callback_data="old_lage")
+                    markup_inline.row(buttons_old, buttons_next)
+                if db.check_date_tarife(user_id, callback_query.data) is None:
+                    pay_money_buttons = types.InlineKeyboardButton(text=cfg.pay_money_channels, callback_data='pay_money_channels')
+                    markup_inline.add(pay_money_buttons)
+                back_channels = types.InlineKeyboardButton(text=cfg.back_channels, callback_data='back_channels')
+                markup_inline.add(back_channels)
+                await callback_query.message.edit_caption(caption=cfg.group_text_use, reply_markup=markup_inline, parse_mode=types.ParseMode.MARKDOWN)
+        elif callback_query.data == "old_page":
+            if channels_page == page_here:
+                await callback_query.answer(cfg.error_page_old, show_alert=True)
+            else:
+                channels = db.select_channels_with_number(number_group)
+                markup_inline = types.InlineKeyboardMarkup(row_width=2)
+                channels_count = len(channels)
+                channels_page = fnc.get_category(channels_count)
+                page_here = 1
+                for channel in channels[:channels_count + 10]:
+                    buttons = types.InlineKeyboardButton(text=channel, callback_data=channel)
+                    markup_inline.row(buttons)
+                buttons_count = types.InlineKeyboardButton(text=f"Страница {page_here}/{channels_page}", callback_data="page")
+                markup_inline.add(buttons_count)
+                if channels_count > 10:
+                    buttons_next = types.InlineKeyboardButton(text=cfg.next_page, callback_data="next_page")
+                    buttons_old = types.InlineKeyboardButton(text=cfg.old_page, callback_data="old_lage")
+                    markup_inline.row(buttons_old, buttons_next)
+                if db.check_date_tarife(user_id, callback_query.data) is None:
+                    pay_money_buttons = types.InlineKeyboardButton(text=cfg.pay_money_channels, callback_data='pay_money_channels')
+                    markup_inline.add(pay_money_buttons)
+                back_channels = types.InlineKeyboardButton(text=cfg.back_channels, callback_data='back_channels')
+                markup_inline.add(back_channels)
+                await callback_query.message.edit_caption(caption=cfg.group_text_use, reply_markup=markup_inline, parse_mode=types.ParseMode.MARKDOWN)
         elif callback_query.data == "back_channels":
             await state.reset_state()
             markup_inline = types.InlineKeyboardMarkup(row_width=1)
