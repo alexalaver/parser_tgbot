6be108aa7620b14037a4ec3a4b7b64c6e388eac7
alexalaver <alex.alaverdyan.04@gmail.com>
2023-11-10 18:17:42 +0400

    new_version optimizated

--- main.py
+++ main.py
@@ -49,8 +49,9 @@ async def search_and_forward():
 
             group = groups[num]
             user_id, chat_ids, keywords = group[0], group[2], group[5]
+            print(chat_ids)
             chat_ids = [item for item in chat_ids if item.endswith('✅')]
-
+            print(chat_ids)
             for chat_id in chat_ids:
                 last_id = last_message_ids.get(chat_id[:-2], 0)
                 messages_to_check = 10
