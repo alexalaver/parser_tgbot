e912d0a7ba9351698013d1b8424cf9e415a3433e
alexalaver <alex.alaverdyan.04@gmail.com>
2023-11-12 23:07:35 +0400

    bugfix parser

--- main.py
+++ main.py
@@ -50,61 +50,56 @@ async def search_and_forward():
     groups = db.select_all_channels_group()
 
     while True:
-        try:
-            if not groups:
-                await asyncio.sleep(10)
-                continue
-
-            if await check_for_new_groups(groups_count):
-                groups_count = len(groups)
-                num = 0
-
-            group = groups[num]
-            user_id, chat_ids, keywords = group[0], group[2], group[5]
-            chat_ids = [item for item in chat_ids if item.endswith('✅')]
-
-            for chat_id in chat_ids:
-                trimmed_chat_id = chat_id[:-2]
-
-                last_id = last_message_ids.get(trimmed_chat_id, 0)
-                messages_to_check = 10
-                forced_check = num == 0 and last_id == 0
-
-                try:
-                    async for message in telethon_client.iter_messages(trimmed_chat_id, offset_id=last_id - messages_to_check, limit=messages_to_check, reverse=True):
-                        if message.text and any(keyword.lower() in message.text.lower() for keyword in keywords):
-                            message_key = (user_id, message.id)
-                            if message_key not in messages_sent or forced_check:
-                                sender = await message.get_sender()
-                                sender_identifier = f"@{sender.username}" if sender and sender.username else "Анонимный пользователь"
-
-                                message_text = f"{message.text}\n\nИз чата {trimmed_chat_id}\n\nСообщение от {sender_identifier}"
-                                await bot.send_message(user_id, message_text, parse_mode=types.ParseMode.HTML)
-                                print(f"Message sent to user {user_id}: {message.text}")
-                                messages_sent[message_key] = True
-                                await asyncio.sleep(5)
-
-                except FloodWaitError as e:
-                    wait_time = e.seconds
-                    print(f"Flood wait error on chat {trimmed_chat_id}, waiting for {wait_time} seconds.")
-                    await asyncio.sleep(wait_time)
-                    continue
-                except Exception as inner_e:
-                    print(f"Error processing messages in chat {trimmed_chat_id}: {inner_e}")
-                    break
-
-                finally:
-                    messages = await telethon_client.get_messages(trimmed_chat_id, limit=1)
-                    if messages:
-                        last_message_ids[trimmed_chat_id] = messages[0].id
-
-            num += 1
-            if num >= len(groups):
-                num = 0
-
-        except Exception as e:
-            print(f"Unexpected error: {e}")
+        if not groups:
             await asyncio.sleep(10)
+            continue
+
+        if await check_for_new_groups(groups_count):
+            groups_count = len(groups)
+            num = 0
+
+        group = groups[num]
+        user_id, chat_ids, keywords = group[0], group[2], group[5]
+        chat_ids = [item for item in chat_ids if item.endswith('✅')]
+
+        for chat_id in chat_ids:
+            trimmed_chat_id = chat_id[:-2]
+
+            last_id = last_message_ids.get(trimmed_chat_id, 0)
+            messages_to_check = 10
+            forced_check = num == 0 and last_id == 0
+
+            try:
+                async for message in telethon_client.iter_messages(trimmed_chat_id, offset_id=last_id - messages_to_check, limit=messages_to_check, reverse=True):
+                    if message.text and any(keyword.lower() in message.text.lower() for keyword in keywords):
+                        message_key = (user_id, message.id)
+                        if message_key not in messages_sent or forced_check:
+                            sender = await message.get_sender()
+                            sender_identifier = f"@{sender.username}" if sender and sender.username else "Анонимный пользователь"
+
+                            message_text = f"{message.text}\n\nИз чата {trimmed_chat_id}\n\nСообщение от {sender_identifier}"
+                            await bot.send_message(user_id, message_text, parse_mode=types.ParseMode.HTML)
+                            print(f"Message sent to user {user_id}: {message.text}")
+                            messages_sent[message_key] = True
+                            await asyncio.sleep(5)
+
+            except FloodWaitError as e:
+                wait_time = e.seconds
+                print(f"Flood wait error on chat {trimmed_chat_id}, waiting for {wait_time} seconds.")
+                await asyncio.sleep(wait_time)
+                continue
+            except Exception as inner_e:
+                print(f"Error processing messages in chat {trimmed_chat_id}: {inner_e}")
+                break
+
+            finally:
+                messages = await telethon_client.get_messages(trimmed_chat_id, limit=1)
+                if messages:
+                    last_message_ids[trimmed_chat_id] = messages[0].id
+
+        num += 1
+        if num >= len(groups):
+            num = 0
 
         await asyncio.sleep(20)
 
