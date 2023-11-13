950db3ece582292fed1d532294c5491f417804f6
alexalaver <alex.alaverdyan.04@gmail.com>
2023-11-14 02:05:43 +0400

    bugfix parser

--- datbas.py
+++ datbas.py
@@ -250,8 +250,10 @@ class Data:
             return self.cursor.fetchone()[0]
 
     def update_message_id(self, message_ids):
+        message_ids_list = [msg_id[1] for msg_id in message_ids]
+        message_ids_array = list(map(int, message_ids_list))
         with self.connect:
-            self.cursor.execute("UPDATE message_id SET message_ids = %s", (message_ids,))
+            self.cursor.execute("UPDATE message_id SET message_ids = %s WHERE some_condition", (message_ids_array,))
             self.connect.commit()
 
     def select_message_id(self):
