3a048cd505702e38bfcf11ca5dfcd32d2e3b7d42
alexalaver <alex.alaverdyan.04@gmail.com>
2023-11-13 23:49:07 +0400

    bugfix parser

--- main.py
+++ main.py
@@ -108,7 +108,6 @@ async def search_and_forward():
                     new_channels = [new_callback if item == chat_id else item for item in chat_ids]
                     db.update_all_channels(number_group, new_channels)
                     await asyncio.sleep(2)
-                    break
                 finally:
                     messages = await telethon_client.get_messages(trimmed_chat_id, limit=1)
                     if messages:
