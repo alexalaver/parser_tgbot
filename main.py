affd609df15ba3ab9296c4074cf5e6ff335ab27f
alexalaver <alex.alaverdyan.04@gmail.com>
2023-11-14 18:30:33 +0400

    bugfix parser

--- main.py
+++ main.py
@@ -134,7 +134,7 @@ async def search_and_forward():
                                     if message_key[1] not in messages_sent or forced_check:
                                         sender = await message.get_sender()
                                         sender_identifier = f"@{sender.username}" if sender else "Анонимный пользователь"
-                                        link_message = f"t.me/{trimmed_chat_id[1:]}"
+                                        link_message = f"t.me/{trimmed_chat_id[1:]}/{str(message.id)}"
                                         message_text = f"Обнаружено ключевое слово\n\nЧат: {trimmed_chat_id}\n\nПользователь: {sender_identifier}\n\nЗапрос: {keyword}\n\nСсылка на сообщение: {link_message}\n\nТекст:\n{message.text}"
                                         await bot.send_message(user_id, message_text, parse_mode=types.ParseMode.HTML)
                                         print(f"Message sent to user {user_id}: {message.text}")
