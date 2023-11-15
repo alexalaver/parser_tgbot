c72d842e995356a6db6f810a2c20efdcba3642e8
alexalaver <alex.alaverdyan.04@gmail.com>
2023-11-15 17:48:53 +0400

    new_close

--- main.py
+++ main.py
@@ -175,10 +175,9 @@ async def search_and_forward_close_group():
             messages_sent = db.select_message_id(number_group)
             for chat_id in chat_ids:
                 trimmed_chat_ids = chat_id[:-2]
-                chat_ad = await telethon_client.get_entity(trimmed_chat_ids)
-                trimmed_chat_id = chat_ad.id
-
                 try:
+                    chat_ad = await telethon_client.get_entity(trimmed_chat_ids)
+                    trimmed_chat_id = chat_ad.id
                     last_id = last_message_ids.get(trimmed_chat_id, 0)
                     print(f"link - {trimmed_chat_ids}\nchat_id - {trimmed_chat_id}")
                     messages_to_check = 30
