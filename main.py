212eeae29f651e13dd46afc8d21617c87b3ced66
alexalaver <alex.alaverdyan.04@gmail.com>
2023-11-16 23:02:40 +0400

    new_closeA

--- main.py
+++ main.py
@@ -96,7 +96,7 @@ async def search_and_forward():
                     async for message in telethon_client.iter_messages(chat_check_id, offset_id=last_id - messages_to_check, limit=messages_to_check, reverse=True):
                         if message.text:
                             for keyword in keywords:
-                                print(f"message_test")
+                                print(f"keyword - {keyword}\n{message.text}")
                                 if keyword.lower() in message.text.lower():
                                     message_key = [chat_check_id, message.id]
                                     if message_key not in messages_sent or forced_check:
@@ -115,9 +115,6 @@ async def search_and_forward():
                                         new_callback = chat_id[:-1] + '✅'
                                         new_channels = [new_callback if item == chat_id else item for item in all_channels]
                                         db.update_all_channels(number_group, new_channels)
-                                        messages = await telethon_client.get_messages(trimmed_chat_id, limit=1)
-                                        if messages:
-                                            last_message_ids[trimmed_chat_id] = messages[0].id
                                     break
 
                 except FloodWaitError as e:
@@ -131,6 +128,10 @@ async def search_and_forward():
                     new_channels = [new_callback if item == chat_id else item for item in all_channels]
                     db.update_all_channels(number_group, new_channels)
                     await asyncio.sleep(2)
+                finally:
+                    messages = await telethon_client.get_messages(trimmed_chat_id, limit=1)
+                    if messages:
+                        last_message_ids[trimmed_chat_id] = messages[0].id
 
             num += 1
             if num >= len(groups):
