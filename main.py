a39bfd6c70551e8f946792bcba3302ba5836480b
alexalaver <alex.alaverdyan.04@gmail.com>
2023-11-18 02:50:49 +0400

    panel_adm

--- main.py
+++ main.py
@@ -924,7 +924,7 @@ async def other(message: types.Message):
 
 async def on_startup(_):
     # asyncio.create_task(search_and_forward())
-    asyncio.create_task(search_and_forward_close_group())
+    # asyncio.create_task(search_and_forward_close_group())
 
 if __name__ == "__main__":
     executor.start_polling(dp, skip_updates=True, on_startup=on_startup)
