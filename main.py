d86a66fa04309f2c54330e6cb2c90c2df686cdc0
alexalaver <alex.alaverdyan.04@gmail.com>
2023-11-18 18:33:17 +0400

    panel_adm

--- main.py
+++ main.py
@@ -185,12 +185,13 @@ async def search_and_forward_close_group():
                                     for keyword in keywords:
                                         if keyword.lower() in message.text.lower():
                                             message_key = [trimmed_chat_ids, message.id]
-                                            if message_key not in messages_sent:
+                                            if message_key not in messages_sent or forced_check:
                                                 sender = await message.get_sender()
                                                 sender_identifier = f"@{sender.username}" if sender else "Анонимный пользователь"
                                                 message_text = f"Обнаружено ключевое слово\n\nЧат: {links}\n\nПользователь: {sender_identifier}\n\nЗапрос: {keyword}\n\nСсылка на сообщение: Чат закрыт.\n\nТекст:\n{message.text}"
                                                 await bot.send_message(user_id, message_text, parse_mode=types.ParseMode.HTML)
                                                 print(f"Message sent to user {user_id}: {message.text}")
+                                                db.update_all_message_ids(number_group, message_key)
                                                 await asyncio.sleep(2)
                                             break
 
