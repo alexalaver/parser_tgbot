b2fa7411ed57c8eabe7077d56ac17bcf77f9c4c0
alexalaver <alex.alaverdyan.04@gmail.com>
2023-11-18 20:40:42 +0400

    panel_adm

--- main.py
+++ main.py
@@ -94,7 +94,7 @@ async def search_and_forward():
                             for keyword in keywords:
                                 if keyword.lower() in message.text.lower():
                                     message_key = [chat_check_id, message.id]
-                                    if message_key not in messages_sent or forced_check:
+                                    if message_key not in messages_sent:
                                         sender = await message.get_sender()
                                         if "http" in trimmed_chat_id:
                                             link_message = f"{trimmed_chat_id}/{str(message.id)}"
@@ -634,12 +634,11 @@ async def parsers_use_1_button(callback_query: types.CallbackQuery, state: FSMCo
                 markup_inline = types.InlineKeyboardMarkup(row_width=1)
                 btn_inline1 = types.InlineKeyboardButton(cfg.menu_button, callback_data='menu_after_pay')
                 markup_inline.add(btn_inline1)
-                all_channels = db.select_channels_with_number(number_group)
-                channels_link = [item for item in all_channels if len(item) >= 13 and item[13] == '+']
+                channels_link = [item for item in new_channels if len(item) >= 13 and item[13] == '+']
                 for channel_link in channels_link:
                     new_callback = channel_link[:-1] + '⏳'
-                    new_channels = [new_callback if item == channel_link else item for item in all_channels]
-                    db.update_all_channels(number_group, new_channels)
+                    new_chendel = [new_callback if item == channel_link else item for item in new_channels]
+                    db.update_all_channels(number_group, new_chendel)
                 await check_private_channel(new_chan, user_id, number_group)
                 await state.finish()
                 await callback_query.message.delete()
