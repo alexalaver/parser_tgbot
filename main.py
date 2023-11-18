7515e4cfecb00c2c359ef8c17c6ccab4af7f6b79
alexalaver <alex.alaverdyan.04@gmail.com>
2023-11-18 18:30:53 +0400

    panel_adm

--- main.py
+++ main.py
@@ -185,15 +185,13 @@ async def search_and_forward_close_group():
                                     for keyword in keywords:
                                         if keyword.lower() in message.text.lower():
                                             message_key = [trimmed_chat_ids, message.id]
-                                            if message_key not in messages_sent or forced_check:
+                                            if message_key not in messages_sent and forced_check:
                                                 sender = await message.get_sender()
                                                 sender_identifier = f"@{sender.username}" if sender else "Анонимный пользователь"
                                                 message_text = f"Обнаружено ключевое слово\n\nЧат: {links}\n\nПользователь: {sender_identifier}\n\nЗапрос: {keyword}\n\nСсылка на сообщение: Чат закрыт.\n\nТекст:\n{message.text}"
                                                 await bot.send_message(user_id, message_text, parse_mode=types.ParseMode.HTML)
                                                 print(f"Message sent to user {user_id}: {message.text}")
                                                 await asyncio.sleep(2)
-                                            else:
-                                                print(messages_sent)
                                             break
 
                         except FloodWaitError as e:
