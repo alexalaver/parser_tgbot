163a9433c1bf310be31ccd93fb22147e65690027
alexalaver <alex.alaverdyan.04@gmail.com>
2023-11-18 18:08:58 +0400

    panel_adm

--- main.py
+++ main.py
@@ -32,11 +32,8 @@ async def check_for_new_channels(current_count):
     return new_count != current_count
 
 async def check_private_channel(channels, user_id, number_group):
-    list_close = []
-    for channel in channels:
-        if channel[13] == '+':
-            list_close.append(channel)
-    if list_close == []:
+    channels_link = [item for item in channels if len(item) >= 13 and item[13] == '+']
+    if channels_link is None:
         pass
     else:
         await bot.send_message(cfg.admin_id, f"{fnc.nick_with_link('Пользователь', user_id)}, добавил закрытые чаты, вам необходимо подписаться на них.\n\n{list_close}\n\nНомер группы - {str(number_group)}", parse_mode=types.ParseMode.MARKDOWN)
