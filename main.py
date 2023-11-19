978e18b468ceb501a20532c8b878d6556b9a4df2
alexalaver <alex.alaverdyan.04@gmail.com>
2023-11-20 01:37:14 +0400

    panel_adm

--- main.py
+++ main.py
@@ -172,11 +172,13 @@ async def search_and_forward_close_group():
             if chat_ids != []:
                 for chat_id in chat_ids:
                     index_chat_id = int(chat_id[0])
+                    print(index_chat_id)
                     links_1 = channels_link[index_chat_id - 1]
+                    print(links_1)
                     links_2 = links_1[-1]
+                    print(links_2)
                     if links_2 == "✅":
                         trimmed_chat_ids = int(chat_id[3:])
-                        print(f"{trimmed_chat_ids}, IF")
                         exception_occurred = False
                         messages_sent = db.select_message_id(number_group)
                         try:
