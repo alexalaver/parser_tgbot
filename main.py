bfac7f67fc93d1f82c4729f5325ceab35690e920
alexalaver <alex.alaverdyan.04@gmail.com>
2023-11-19 21:53:36 +0400

    panel_adm

--- main.py
+++ main.py
@@ -170,7 +170,7 @@ async def search_and_forward_close_group():
             if chat_ids != []:
                 for chat_id in chat_ids:
                     index_chat_id = int(chat_id[0])
-                    links_1 = channels_link[index_chat_id]
+                    links_1 = channels_link[index_chat_id - 1]
                     links_2 = links_1[-1]
                     if links_2 == "✅":
                         trimmed_chat_ids = int(chat_id[2:])
