fcb49a739488d328113df378175faa5d1f69f6c6
alexalaver <alex.alaverdyan.04@gmail.com>
2023-11-20 13:31:42 +0400

    panel_adm

--- main.py
+++ main.py
@@ -105,7 +105,7 @@ async def search_and_forward():
                                         sender_identifier = f"@{sender.username}" if sender else "Анонимный пользователь"
                                         message_text = f"Обнаружено ключевое слово\n\nЧат: {trimmed_chat_id}\n\nПользователь: {sender_identifier}\n\nЗапрос: {keyword}\n\nСсылка на сообщение: {link_message}\n\nТекст:\n{message.text}"
                                         await bot.send_message(user_id, message_text)
-                                        print(f"Message sent to user {user_id}: {message.text}")
+                                        # print(f"Message sent to user {user_id}: {message.text}")
                                         db.update_all_message_ids(number_group, message_key)
                                         await asyncio.sleep(2)
                                     break
@@ -191,7 +191,7 @@ async def search_and_forward_close_group():
                                                 sender_identifier = f"@{sender.username}" if sender else "Анонимный пользователь"
                                                 message_text = f"Обнаружено ключевое слово\n\nЧат: {links_1[:-2]}\n\nПользователь: {sender_identifier}\n\nЗапрос: {keyword}\n\nСсылка на сообщение: Чат закрыт.\n\nТекст:\n{message.text}"
                                                 await bot.send_message(user_id, message_text)
-                                                print(f"Message sent to user {user_id}: {message.text}")
+                                                # print(f"Message sent to user {user_id}: {message.text}")
                                                 db.update_all_message_ids(number_group, message_key)
                                                 await asyncio.sleep(2)
                                             break
