f9cdcf7b961e29ac3e418532e618b35816e7aa8a
alexalaver <alex.alaverdyan.04@gmail.com>
2023-11-20 15:55:43 +0400

    panel_adm

--- main.py
+++ main.py
@@ -319,36 +319,32 @@ async def process_phone(message: types.Message, state: FSMContext):
     async with state.proxy() as data:
         data['phone'] = message.text
 
-    client = TelegramClient(StringSession(), cfg.API_ID, cfg.API_HASH)
-    await client.connect()
-
-    try:
-        result = await client.send_code_request(data['phone'])
-        data['phone_code_hash'] = result.phone_code_hash  # Сохраняем phone_code_hash
-        await Form.next()
-        await message.reply("Код отправлен. Введите код из сообщения Telegram.")
-    except Exception as e:
-        await message.reply(f'Ошибка: {e}')
-        await state.finish()
+    # Создаем экземпляр клиента здесь и используем его для отправки кода
+    with TelegramClient(StringSession(), cfg.API_ID, cfg.API_HASH) as client:
+        try:
+            result = await client.send_code_request(data['phone'])
+            data['phone_code_hash'] = result.phone_code_hash
+            await Form.next()
+            await message.reply("Код отправлен. Введите код из сообщения Telegram.")
+        except Exception as e:
+            await message.reply(f'Ошибка: {e}')
+            await state.finish()
 
 @dp.message_handler(state=Form.code)
 async def process_code(message: types.Message, state: FSMContext):
     async with state.proxy() as data:
         data['code'] = message.text
 
-    client = TelegramClient(StringSession(), cfg.API_ID, cfg.API_HASH)
-    await client.connect()
-
-    try:
-        # Используем сохраненный phone_code_hash для аутентификации
-        await client.sign_in(data['phone'], data['code'], phone_code_hash=data['phone_code_hash'])
-        string_session = client.session.save()
-        await message.reply(f'Ваша StringSession: {string_session}')
-    except Exception as e:
-        await message.reply(f'Ошибка: {e}')
-    finally:
-        await client.disconnect()
-        await state.finish()
+    # Создаем экземпляр клиента здесь и используем его для аутентификации
+    with TelegramClient(StringSession(), cfg.API_ID, cfg.API_HASH) as client:
+        try:
+            await client.sign_in(data['phone'], data['code'], phone_code_hash=data['phone_code_hash'])
+            string_session = client.session.save()
+            await message.reply(f'Ваша StringSession: {string_session}')
+        except Exception as e:
+            await message.reply(f'Ошибка: {e}')
+        finally:
+            await state.finish()
 
 #...................
 
