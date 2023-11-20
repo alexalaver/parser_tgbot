ba3a3599c409f23c1b131e103a08dbab70776d88
alexalaver <alex.alaverdyan.04@gmail.com>
2023-11-20 15:58:29 +0400

    panel_adm

--- main.py
+++ main.py
@@ -314,39 +314,38 @@ async def send_welcome(message: types.Message):
     await message.reply("Привет! Отправьте свой номер телефона для получения StringSession.")
     await Form.phone.set()
 
-@dp.message_handler(state=Form.phone)
-async def process_phone(message: types.Message, state: FSMContext):
-    async with state.proxy() as data:
-        data['phone'] = message.text
-
-    client = TelegramClient(StringSession(), cfg.API_ID, cfg.API_HASH)
-    await client.connect()
-
-    try:
-        await client.send_code_request(data['phone'])
-        await Form.next()
-        await message.reply("Код отправлен. Введите код из сообщения Telegram.")
-    except Exception as e:
-        await message.reply(f'Ошибка: {e}')
-        await state.finish()
-
 @dp.message_handler(state=Form.code)
 async def process_code(message: types.Message, state: FSMContext):
     async with state.proxy() as data:
         data['code'] = message.text
 
-    client = TelegramClient(StringSession(), cfg.API_ID, cfg.API_HASH)
-    await client.connect()
+    # Создаем экземпляр клиента здесь и используем его для аутентификации
+    with TelegramClient(StringSession(), cfg.API_ID, cfg.API_HASH) as client:
+        try:
+            await client.sign_in(data['phone'], data['code'], phone_code_hash=data['phone_code_hash'])
+            string_session = client.session.save()
+            await message.reply(f'Ваша StringSession: {string_session}')
+        except Exception as e:
+            await message.reply(f'Ошибка: {e}')
+        finally:
+            await state.finish()
 
-    try:
-        await client.sign_in(data['phone'], data['code'])
-        string_session = client.session.save()
-        await message.reply(f'Ваша StringSession: {string_session}')
-    except Exception as e:
-        await message.reply(f'Ошибка: {e}')
-    finally:
-        await client.disconnect()
-        await state.finish()
+@dp.message_handler(state=Form.phone)
+async def process_phone(message: types.Message, state: FSMContext):
+    async with state.proxy() as data:
+        data['phone'] = message.text
+        logger.info(f"Получен номер телефона: {data['phone']}")
+
+    with TelegramClient(StringSession(), cfg.API_ID, cfg.API_HASH) as client:
+        try:
+            result = await client.send_code_request(data['phone'])
+            data['phone_code_hash'] = result.phone_code_hash
+            await Form.next()
+            await message.reply("Код отправлен. Введите код из сообщения Telegram.")
+        except Exception as e:
+            logger.error(f'Ошибка при отправке кода: {e}')
+            await message.reply(f'Ошибка: {e}')
+            await state.finish()
 
 #...................
 
