551b23470bf1c37e47d43b6b7b6820ee734d1a86
alexalaver <alex.alaverdyan.04@gmail.com>
2023-11-25 19:45:24 +0400

    test

--- main.py
+++ main.py
@@ -26,7 +26,6 @@ db = Data("192.168.1.37", "5432", "pars_db", "pars_user", "pars_pwd")
 #     print("String Session:", client.session.save())
 
 telethon_client = TelegramClient(StringSession(cfg.STRING_SESSION), cfg.API_ID, cfg.API_HASH)
-client = TelegramClient(StringSession(), cfg.API_ID, cfg.API_HASH)
 
 async def check_for_new_groups(current_count):
     new_count = len(db.select_all_channels_group())
@@ -976,6 +975,7 @@ async def add_chat_ids_num_2(message: types.Message, state: FSMContext):
 @dp.message_handler(state=Create_account_autoposting.create_autoposting_1)
 async def process_phone(message: types.Message, state: FSMContext):
     user_id = message.from_user.id
+    client = TelegramClient(StringSession(), cfg.API_ID, cfg.API_HASH)
     if message.text == cfg.cancel_creategroup:
         user_id = message.from_user.id
         markup_reply = types.ReplyKeyboardMarkup(row_width=2, resize_keyboard=True, one_time_keyboard=False)
