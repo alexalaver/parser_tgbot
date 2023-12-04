2abe98cca2736a5a22a2afcaffda6e43a47d27f5
alexalaver <alex.alaverdyan.04@gmail.com>
2023-12-05 00:04:39 +0400

    test

--- main.py
+++ main.py
@@ -1035,6 +1035,7 @@ async def account_use_1_button(callback_query: types.CallbackQuery, state: FSMCo
         user_id = callback_query.from_user.id
         data = await state.get_data()
         number_group = data.get('number_account')
+        print(number_group)
         channels = db.select_chats_account_with_number(number_group)
         check_tarife = db.check_date_tarife_account_for_number(number_group)
         group_name = db.select_account_name_for_number_group(number_group)
