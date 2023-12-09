83b8217ca895a2dcc3d6af07f7ed95db11689e64
alexalaver <alex.alaverdyan.04@gmail.com>
2023-12-10 02:10:07 +0400

    update

--- main.py
+++ main.py
@@ -955,7 +955,7 @@ async def buttons_callback(callback_query: types.CallbackQuery, state: FSMContex
             elif callback_query.data == "no_delete_autoposting":
                 post_name1 = str(callback_query.message)
                 await bot.send_message(user_id, str(post_name1))
-                post_name2 = re.findall(r"'([^']*)'", post_name1)
+                post_name2 = re.findall(r"'([^']*)'", post_name1[0])
                 await bot.send_message(user_id, str(post_name2))
                 channels = db.select_chats_post(user_id, post_name2)
                 markup_inline = types.InlineKeyboardMarkup(row_width=1)
