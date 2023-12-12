9fbb7038e5d91273ba37f121e74f14c7d71acf78
alexalaver <alex.alaverdyan.04@gmail.com>
2023-12-13 01:44:04 +0400

    update

--- main.py
+++ main.py
@@ -923,8 +923,8 @@ async def buttons_callback(callback_query: types.CallbackQuery, state: FSMContex
                     markup_inline = types.InlineKeyboardMarkup(row_width=1)
                     btn_inline1 = types.InlineKeyboardButton(cfg.menu_button, callback_data='menu_after_pay_parser')
                     markup_inline.add(btn_inline1)
-                    all_channels = db.select_channels_name_with_number(number_group_parser)
-                    all_channels_name = db.select_channels_with_number(number_group_parser)
+                    all_channels = db.select_channels_with_number(number_group_parser)
+                    all_channels_name = db.db.select_channels_name_with_number(number_group_parser)
                     updated_a = [x[:-1] + '⏳' if len(x) >= 13 and x[13] == '+' else x for x in all_channels]
                     db.update_all_channels(number_group_parser, updated_a)
                     paired_channels = zip(updated_a, all_channels_name)
