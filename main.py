ec72acebaafe0a244c0b6cd53b46227a02284e74
alexalaver <alex.alaverdyan.04@gmail.com>
2023-12-14 01:06:22 +0400

    update

--- main.py
+++ main.py
@@ -1621,6 +1621,7 @@ async def add_chat_ids_num_2(message: types.Message, state: FSMContext):
             except Exception:
                 await message.answer(cfg.error_add_ids, reply_markup=markup_reply, parse_mode=types.ParseMode.MARKDOWN)
                 await Add_chat_ids.panel_adm.set()
+
         old_chat_list = db.select_chat_ids(number_group) or []
         new_lst = old_chat_list + new_lst
         db.add_chat_ids(int(number_group), new_lst)
