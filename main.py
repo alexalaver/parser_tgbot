8eacba2003a44e327351b376593d015335506729
alexalaver <alex.alaverdyan.04@gmail.com>
2023-12-16 01:42:18 +0400

    updates new version

--- main.py
+++ main.py
@@ -304,6 +304,7 @@ async def autoposting_forward():
                                             print(f"[ERROR EXCEPTION] {erri}")
                                 time_in_60_minutes = formated_base + datetime.timedelta(minutes=1440)
                                 formatted_date_new = time_in_60_minutes.strftime("%Y-%m-%d %H:%M:%S")
+                                print(formatted_date_new)
                                 new_channels = [formatted_date_new if item == date_bet else item for item in chat_id]
                                 print(new_channels)
                                 new_updates = [new_channels if item == chat_id else item for item in chat_idn]
