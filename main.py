4b4895b25d40f39d6de8698e1d6f6003d6c96775
alexalaver <alex.alaverdyan.04@gmail.com>
2023-12-16 01:51:57 +0400

    updates new version

--- main.py
+++ main.py
@@ -286,15 +286,13 @@ async def autoposting_forward():
             formated_base = datetime.datetime.strptime(data_end, "%Y-%m-%d %H:%M:%S")
             if formated_base > current_date:
                 if chat_ids != []:
-                    current_date = datetime.datetime.now()
+                    current_date = datetime.datetime.now(moscow_tz)
                     for chat_id in chat_ids:
                         formated_chat_id = chat_id[0][:-2]
                         date_betw = chat_id[1:]
                         for date_bet in date_betw:
                             formated_base = datetime.datetime.strptime(date_bet, "%Y-%m-%d %H:%M:%S")
-                            print("right 1")
                             if current_date > formated_base:
-                                print("right 2")
                                 async with TelegramClient(StringSession(string_session), cfg.API_ID, cfg.API_HASH) as telethon_client_autoposting:
                                         try:
                                             await telethon_client_autoposting.forward_messages(entity=formated_chat_id, messages=int(message_id), from_peer=channel_tag)
