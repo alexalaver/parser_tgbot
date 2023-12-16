de33ea44be6afde51c122374956b2242782ea267
alexalaver <alex.alaverdyan.04@gmail.com>
2023-12-16 18:19:37 +0400

    add_time_chat

--- main.py
+++ main.py
@@ -1220,7 +1220,7 @@ async def buttons_callback(callback_query: types.CallbackQuery, state: FSMContex
                     channels_name = db.select_chats_name_account_with_number(number_group_autoposting)
                     new_channels = [[channel[0][:-2] + " ✅"] for channel in channels]
                     new_chats_name = [newer[:-2] + " ✅" for newer in channels_name]
-                    print(new_chats_name)
+                    print(new_channels)
                     db.update_all_chats_name_account(number_group_autoposting, new_chats_name)
                     db.update_balance(user_id, money_oplata)
                     channels_len = len(channels)
