df996c7ec44b38b10dd4ede91d07668e601d428c
alexalaver <alex.alaverdyan.04@gmail.com>
2023-12-16 18:21:36 +0400

    add_time_chat

--- main.py
+++ main.py
@@ -1334,7 +1334,8 @@ async def add_time_autoposting_chat_text(message: types.Message, state: FSMConte
                         result = selected_sublist[0] if selected_sublist else []
                         result.append(formatted_datetime)
                         new_updates = [result if item[0] == settings_callback_data else item for item in post_names]
-                        db.update_chat_idn_autoposting(new_updates, number_group_autoposting)
+                        json_data = json.dumps(new_updates)
+                        db.update_chat_idn_autoposting(json_data, number_group_autoposting)
                         await message.answer(cfg.add_time_text_finish, reply_markup=markup_reply)
                         await state.finish()
                     else:
