809a4b4dca977f7a93f09cfc6bd8a9384b9457e3
alexalaver <alex.alaverdyan.04@gmail.com>
2023-12-19 01:30:58 +0400

    new_update

--- main.py
+++ main.py
@@ -957,7 +957,7 @@ async def buttons_callback(callback_query: types.CallbackQuery, state: FSMContex
                     if channels_id != [(None,)]:
                         for channel_id in channels_id:
                             for channel_link in channels_link:
-                                for lst_with_i in lst_with_id
+                                for lst_with_i in lst_with_id:
                                     if channel_id[1] != lst_with_i[1]:
                                         if channel_id[1] == channel_link[:-2]:
                                             parts = channel_id[0].split(') ')
