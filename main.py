b0cae08fee714850769799c3e14c6a07ea9a5285
alexalaver <alex.alaverdyan.04@gmail.com>
2023-12-19 00:38:07 +0400

    new_update

--- main.py
+++ main.py
@@ -972,6 +972,8 @@ async def buttons_callback(callback_query: types.CallbackQuery, state: FSMContex
                             index = int(new_lst_with_ids[0]) - 1
                             channels_link[index] = channels_link[index].replace("⏳", "✅")
                             owner_lst = [next((full_word for full_word in channels_link if full_word[:-2] == word[:-2]), word) for word in all_channels]
+                            print(owner_lst)
+                            print(lst_with_id)
                             db.update_all_channels(number_group_parser, owner_lst)
                             db.add_chat_ids(int(number_group_parser), lst_with_id)
                         all_channels = db.select_channels_with_number(number_group_parser)
