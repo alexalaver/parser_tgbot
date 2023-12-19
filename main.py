aee9aa358921d0d5a705aeebc378c5f94da89139
alexalaver <alex.alaverdyan.04@gmail.com>
2023-12-20 03:49:07 +0400

    beta 1.0

--- main.py
+++ main.py
@@ -105,22 +105,22 @@ async def search_and_forward():
                         exception_occurred = False
                         messages_sent = db.select_message_id(number_group)
                         for chat_id_name in chat_names:
-                        try:
-                            chat_select_id = await telethon_client.get_entity(trimmed_chat_id)
-                            chat_check_id = chat_select_id.id
-                            last_id = last_message_ids.get(trimmed_chat_id, 0)
-                            messages_to_check = 30
-                            forced_check = num == 0 and last_id == 0
-                            async for message in telethon_client.iter_messages(chat_check_id, offset_id=last_id - messages_to_check, limit=messages_to_check, reverse=True):
-                                if message.text:
-                                    for keyword in keywords:
-                                        if keyword.lower() in message.text.lower():
-                                            message_key = [chat_check_id, message.id]
-                                            if message_key not in messages_sent:
-                                                sender = await message.get_sender()
-                                                if "http" in trimmed_chat_id:
-                                                    link_message = f"{trimmed_chat_id}/{str(message.id)}"
-                                                else:
+                            try:
+                                chat_select_id = await telethon_client.get_entity(trimmed_chat_id)
+                                chat_check_id = chat_select_id.id
+                                last_id = last_message_ids.get(trimmed_chat_id, 0)
+                                messages_to_check = 30
+                                forced_check = num == 0 and last_id == 0
+                                async for message in telethon_client.iter_messages(chat_check_id, offset_id=last_id - messages_to_check, limit=messages_to_check, reverse=True):
+                                    if message.text:
+                                        for keyword in keywords:
+                                            if keyword.lower() in message.text.lower():
+                                                message_key = [chat_check_id, message.id]
+                                                if message_key not in messages_sent:
+                                                    sender = await message.get_sender()
+                                                    if "http" in trimmed_chat_id:
+                                                        link_message = f"{trimmed_chat_id}/{str(message.id)}"
+                                                    else:
                                                         link_message = f"t.me/{trimmed_chat_id[1:]}/{str(message.id)}"
                                                     sender_identifier = f"@{sender.username}" if sender and sender.username else "Анонимный пользователь"
                                                     escaped_message_text = escape_html(message.text)
