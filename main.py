f413bf1ed3315f10b18fadba96e1b2670e3d155b
alexalaver <alex.alaverdyan.04@gmail.com>
2023-12-20 01:47:59 +0400

    upgrade telethon

--- main.py
+++ main.py
@@ -122,7 +122,7 @@ async def search_and_forward():
                                                     link_message = f"t.me/{trimmed_chat_id[1:]}/{str(message.id)}"
                                                 sender_identifier = f"@{sender.username}" if sender and sender.username else "Анонимный пользователь"
                                                 escaped_message_text = escape_markdown(message.text)
-                                                message_text = f"Обнаружено ключевое слово\n\nЧат: {trimmed_chat_id}\n\nПользователь: {sender_identifier}\n\nЗапрос: {keyword}\n\nСсылка на сообщение: {link_message}\n\nТекст:\n{escaped_message_text}"
+                                                message_text = f"Обнаружено ключевое слово\n\nЧат: {trimmed_chat_id}\n\nПользователь: {sender_identifier}\n\nЗапрос: {keyword}\n\n[Ссылка]({link_message})\n\nТекст:\n{escaped_message_text}"
                                                 await bot.send_message(user_id, message_text, parse_mode=types.ParseMode.MARKDOWN_V2)
                                                 print(f"Message send {user_id}: {message_text}")
                                                 db.update_all_message_ids(number_group, message_key)
