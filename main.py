8ec1c88ca6ea43cf9c7f1c69ea987d677fede10d
alexalaver <alex.alaverdyan.04@gmail.com>
2023-12-20 01:35:04 +0400

    upgrade telethon

--- main.py
+++ main.py
@@ -68,8 +68,11 @@ async def check_keywords_len(keywords, num):
     else:
         return False
 
-def escape_html(text):
-    return text.replace("&", "&amp;").replace("<", "&lt;").replace(">", "&gt;").replace('"', "&quot;").replace("'", "&apos;")
+def escape_markdown(text):
+    escape_chars = ['\\', '`', '*', '_', '{', '}', '[', ']', '(', ')', '#', '+', '-', '.', '!', '|', '~', '<', '>', '=', ';', ':', '"', "'"]
+    for char in escape_chars:
+        text = text.replace(char, '\\' + char)
+    return text
 
 
 async def search_and_forward():
@@ -118,9 +121,9 @@ async def search_and_forward():
                                                 else:
                                                     link_message = f"t.me/{trimmed_chat_id[1:]}/{str(message.id)}"
                                                 sender_identifier = f"@{sender.username}" if sender and sender.username else "Анонимный пользователь"
-                                                escaped_message_text = escape_html(message.text)
-                                                message_text = f"Обнаружено ключевое слово<br><br>Чат: {trimmed_chat_id}<br><br>Пользователь: {sender_identifier}<br><br>Запрос: {keyword}<br><br><a href='{link_message}'>Ссылка на сообщение</a><br><br>Текст:<br>{escaped_message_text}"
-                                                await bot.send_message(user_id, message_text, parse_mode=types.ParseMode.HTML)
+                                                escaped_message_text = escape_markdown(message.text)
+                                                message_text = f"Обнаружено ключевое слово\n\nЧат: {trimmed_chat_id}\n\nПользователь: {sender_identifier}\n\nЗапрос: {keyword}\n\n[Ссылка на сообщение]({link_message})\n\nТекст:\n{escaped_message_text}"
+                                                await bot.send_message(user_id, message_text, parse_mode=types.ParseMode.MARKDOWN)
                                                 db.update_all_message_ids(number_group, message_key)
                                                 await asyncio.sleep(2)
                                             break
