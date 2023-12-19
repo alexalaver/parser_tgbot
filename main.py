ae6c808bb5803e7678067cbcab667abac4d769af
alexalaver <alex.alaverdyan.04@gmail.com>
2023-12-20 01:40:19 +0400

    upgrade telethon

--- main.py
+++ main.py
@@ -117,6 +117,7 @@ async def search_and_forward():
                                                 sender_identifier = f"@{sender.username}" if sender and sender.username else "Анонимный пользователь"
                                                 message_text = f"Обнаружено ключевое слово\n\nЧат: {trimmed_chat_id}\n\nПользователь: {sender_identifier}\n\nЗапрос: {keyword}\n\nСсылка на сообщение: {link_message}\n\nТекст:\n{message.text}"
                                                 await bot.send_message(user_id, message_text)
+                                                print(f"Message send {user_id}: {message_text}")
                                                 db.update_all_message_ids(number_group, message_key)
                                                 await asyncio.sleep(2)
                                             break
