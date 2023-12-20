eb63f158155f451477b7f01f341684c28473d287
alexalaver <alex.alaverdyan.04@gmail.com>
2023-12-20 14:59:02 +0400

    beta 1.0

--- main.py
+++ main.py
@@ -121,7 +121,7 @@ async def search_and_forward():
                                                     link_message = f"t.me/{trimmed_chat_id[1:]}/{str(message.id)}"
                                                     chat_link = f"t.me/{trimmed_chat_id[1:]}"
                                                 sender_identifier = f"@{sender.username}" if sender and sender.username else "Анонимный пользователь"
-                                                if sender_identifier[-3:] != "bot":
+                                                if "bot" not in sender_identifier:
                                                     escaped_message_text = escape_html(message.text)
                                                     message_text = f"Обнаружено ключевое слово\n\n<a href='{chat_link}'>{chat_id_name[:-2]}</a>\n\nПользователь: {sender_identifier}\n\nЗапрос: {keyword}\n\n<a href='{link_message}'>Ссылка на сообщение</a>\n\nТекст:\n{escaped_message_text}"
                                                     await bot.send_message(user_id, message_text, parse_mode=types.ParseMode.HTML)
