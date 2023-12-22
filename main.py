c3462f70e1d98d6e67f44dead9fd6642a2a3e6dc
alexalaver <alex.alaverdyan.04@gmail.com>
2023-12-22 18:10:40 +0400

    new update

--- main.py
+++ main.py
@@ -633,7 +633,7 @@ async def buttons_callback(callback_query: types.CallbackQuery, state: FSMContex
                     markup_inline.add(button)
                 btn_inline1 = types.InlineKeyboardButton(cfg.groups_add_button, callback_data='groups_add_button_parser')
                 markup_inline.add(btn_inline1)
-                await callback_query.message.answer_photo(photo=types.InputFile("img/photo2.jpg"), caption=cfg.parser_text, reply_markup=markup_inline, parse_mode=types.ParseMode.MARKDOWN)
+                await callback_query.message.answer_photo(photo=types.InputFile("img/testphoto.png"), caption=cfg.parser_text, reply_markup=markup_inline, parse_mode=types.ParseMode.MARKDOWN)
             except Exception:
                 await callback_query.answer("Произошла ошибка при нажатии на кнопку Меню для Парсера", show_alert=True)
         elif callback_query.data == "menu_after_pay_autoposting":
