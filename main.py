a5e4ff0e57f17f61e6d77addbfe9b301ee5e53d2
alexalaver <alex.alaverdyan.04@gmail.com>
2023-12-23 01:49:31 +0400

    update

--- main.py
+++ main.py
@@ -510,7 +510,8 @@ async def start(message: types.Message):
         first_name = message.from_user.first_name
         username = message.from_user.username
         if(not db.check_user(user_id)):
-            ids = db.check_numbers_ids()            if ids == 1:
+            ids = db.check_numbers_ids()
+            if ids == 1:
                 return ids
             else:
                 ids += 1
