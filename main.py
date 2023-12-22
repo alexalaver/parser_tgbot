6ddb4df1bd38753b91d7a2f45521a3c44da72d8c
alexalaver <alex.alaverdyan.04@gmail.com>
2023-12-23 01:20:26 +0400

    update

--- main.py
+++ main.py
@@ -29,7 +29,7 @@ logger = logging.getLogger(__name__)
 bot = Bot(cfg.TOKEN)
 dp = Dispatcher(bot, storage=MemoryStorage())
 dp.middleware.setup(LoggingMiddleware())
-db = Data("178.20.41.174", "21", "zues_db", "zeus_user", "zeus828282")
+db = Data("178.20.41.174", "5432", "zues_db", "zeus_user", "zeus828282")
 
 # with TelegramClient(StringSession(), cfg.API_ID, cfg.API_HASH) as client:
 #     print("String Session:", client.session.save())
