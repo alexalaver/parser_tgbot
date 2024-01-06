c633641374522f8079a242f4d55c4ea021e965c4
alexalaver <alex.alaverdyan.04@gmail.com>
2024-01-07 00:07:13 +0400

    new system add post name 1.9

--- main.py
+++ main.py
@@ -2004,12 +2004,12 @@ async def add_post_func_text_5(message: types.Message, state: FSMContext):
                             async with TelegramClient(StringSession(string_session), cfg.API_ID, cfg.API_HASH) as client_add_post:
                                 try:
                                     all_dialogs = await client_add_post.get_dialogs()
-                                    for dialog in all_dialogs:
+                                    paired_channels = zip(all_dialogs, text_line)
+                                    for dialog, chat_name in paired_channels:
                                         print(dialog)
-                                        for chat_name in text_line:
-                                            if dialog.name == chat_name:
-                                                chat_ids.append(str(dialog.id))
-                                                break
+                                        if dialog.name == chat_name:
+                                            chat_ids.append(str(dialog.id))
+                                            break
                                 except ValueError:
                                     bluable = False
                             if bluable == False:
