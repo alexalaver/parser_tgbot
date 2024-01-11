f942eee75714ab0d6534bb23eb0fb85d07a7222e
alexalaver <alex.alaverdyan.04@gmail.com>
2024-01-12 00:20:37 +0400

    new system add post name 5.0

--- main.py
+++ main.py
@@ -421,8 +421,11 @@ async def update_all_groups():
 
                 for group_chat in groups_chat:
                     for name_chat in chat_names:
+                        print(f"{name_chat[-1]} right 4")
                         if name_chat[-1] == "⏳":
+                            print("right 5")
                             if group_chat[0] == name_chat[:-2]:
+                                print("right 6")
                                 index_chat = chat_names.index(name_chat)
                                 new_id_chat = group_chat[1]
                                 chat_ids[index_chat] = new_id_chat + " ✅"
