20205dc4ea261a371789c20a2993d9575b16b741
alexalaver <alex.alaverdyan.04@gmail.com>
2024-01-12 01:49:52 +0400

    new system add post name 6.5

--- main.py
+++ main.py
@@ -104,7 +104,8 @@ def get_user_username(link):
 
 async def get_group_tag(group_id):
     try:
-        group = await telethon_client.get_entity(group_id)
+        group = await telethon_client.get_entity("@testonepublic")
+        print(group.id)
         print(group)
         if group.username:
             return f"https://t.me/{group.username}"
