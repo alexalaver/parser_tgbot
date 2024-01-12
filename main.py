6cdc4f8bd5dbf33138b7ef66ce0996f7e83d8516
alexalaver <alex.alaverdyan.04@gmail.com>
2024-01-13 02:14:43 +0400

    new system add post name 8.8

--- main.py
+++ main.py
@@ -2327,6 +2327,7 @@ async def create_group_func_3(message: types.Message, state: FSMContext):
                         await Create_group.create_group_4.set()
                         for not_find_chat in not_find_chats:
                             text_not_find = "4. В списке предоставленных чатов в базе данных не обнаружены:\n\n" + f"{not_find_chat}\n" + "Напишите тэги/ссылки для данных чатов."
+                        print(f"{names_chats}\n{ids_chats}")
                         await state.update_data(names_chats=names_chats, ids_chats=ids_chats)
                         await message.answer(text_not_find)
                     else:
