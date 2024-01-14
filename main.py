f74a3ffbe1b4119954d352391f9a45db9634952d
alexalaver <alex.alaverdyan.04@gmail.com>
2024-01-15 01:59:36 +0400

    new system add post name "10.0

--- main.py
+++ main.py
@@ -2322,6 +2322,8 @@ async def create_group_func_3(message: types.Message, state: FSMContext):
                                 await state.update_data(names_chats=names_chats, ids_chats=ids_chats)
                                 lens_name -= 1
                                 new_bl = True
+                            print(lens_name)
+                            print(new_bl)
                             if lens_name == 1 and new_bl == False:
                                 booline = False
                                 not_find_chats.append(name_chat)
