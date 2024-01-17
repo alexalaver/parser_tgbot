b522f375682043cfc6a4676d723d1982be1c98d7
alexalaver <alex.alaverdyan.04@gmail.com>
2024-01-18 01:00:41 +0400

    new system add post name "36.0

--- main.py
+++ main.py
@@ -2341,7 +2341,7 @@ async def create_group_func_3(message: types.Message, state: FSMContext):
                     for name_chat in text_line:
                         for name_group in all_groups:
                             print(f"ONE\n{name_chat[:-2]}\n{name_group[0][:-2]}")
-                            if name_chat == name_group[0]:
+                            if name_chat[:-2] == name_group[0][:-2]:
                                 names_chats.append(name_chat)
                                 ids_chats.append(name_group[1])
                                 # print(names_chats)
