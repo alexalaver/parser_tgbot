92c32d8fd6f8dd907bce4e51e5006bdec132b781
alexalaver <alex.alaverdyan.04@gmail.com>
2024-01-19 22:28:29 +0400

    bugfix parsing 24.0

--- main.py
+++ main.py
@@ -173,7 +173,8 @@ async def search_and_forward():
                             last_id = last_message_ids.get(trimmed_chat_id, 0)
                             messages_to_check = 30
                             forced_check = num == 0 and last_id == 0
-                            async for message in telethon_client.iter_messages(trimmed_chat_id, offset_id=last_id - messages_to_check, limit=messages_to_check, reverse=True):
+                            offset_id = 0 if last_id == 0 else last_id - messages_to_check
+                            async for message in telethon_client.iter_messages(trimmed_chat_id, offset_id=offset_id, limit=messages_to_check, reverse=True):
                                 if message.text:
                                     for keyword in keywords:
                                         if keyword.lower() in message.text.lower():
